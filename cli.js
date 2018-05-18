91dc9f8149a3be704d2af0e7b154bb932fa776b4
Richard Littauer <richard+github@burntfen.com>
2018-05-18 13:32:22 -0400

    fix: change argv, add destination opt to comment

--- cli.js
+++ cli.js
@@ -2,9 +2,13 @@
 const fn = require('./')
 
 if (process.argv.length < 3 || !process.argv.every(val => val.match(/(--help|-h)/) === null)) {
-  console.log('Usage: node ' + process.argv[1] + ' <email@address.domain>')
-  console.log('Or:    covgen <email@address.domain> (if installed globally)')
+  console.log('Usage: node ' + process.argv[1] + ' <email@address.domain> [destination]')
+  console.log('Or:    covgen <email@address.domain> [destination] (if installed globally)')
   process.exit(1)
 }
 
-fn(process.argv[2])
+if (process.argv[0] === 'covgen') {
+  fn(process.argv[1], process.argv[2])
+} else {
+  fn(process.argv[2], process.argv[3])
+}
